2bc7bd544dc1eca7994eeba9f4e8858eac1af05e
Isabelle Ferrier <ferrierisabelle@gmail.com>
2019-05-13 12:58:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var recipes, obj;
 
 function updateObjectWithKeyAndValue(object, key, value) {
   var obj = { [prop] : '1' };
-  object[key] = value;
+  Object.assign({}, object, key, value);
   return(obj);
 }
 
