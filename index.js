bab95696e7df96f1d88cf9514a77ded8898450c6
Isabelle Ferrier <ferrierisabelle@gmail.com>
2019-05-13 13:17:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
 }
 
 function deleteFromObjectByKey(obj,key) {
-  var newObj = delete obj[key];
+  var newObj = Object.assign({} obj);
+  delete obj[key];
   return(newObj);
 }
 
